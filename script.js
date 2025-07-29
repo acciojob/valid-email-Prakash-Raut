644f426e8ca25687d781ebd969f11fe94988c93d
Prakash Raut <prakash_raut@outlook.com>
2025-07-29 17:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+  if (!str) return false;
+
+  const regex = /^[\w]+([.-]?[\w]+)*@[\w]+([.-]?[\w]+)*\.[a-z]{2,3}$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
